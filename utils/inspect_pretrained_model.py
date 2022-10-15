fc3e0d9e3bea37f025dba2f071ef17d8dd5f076b
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-10-15 15:18:57 +0000

    updated utils script

--- utils/inspect_pretrained_model.py
+++ utils/inspect_pretrained_model.py
@@ -1,10 +1,9 @@
 import torch
-import os, argparse
+import os, sys, argparse
 import json
 
 def load_torch_checkpoint(checkpoint_path):
     return torch.load(checkpoint_path)
-
 
 def replace_tensors_with_placeholders(checkpoint):
     for k, v in checkpoint.items():
@@ -14,10 +13,32 @@ def replace_tensors_with_placeholders(checkpoint):
             checkpoint[k] = f"torch tensor of shape {list(v.size())}"
     return checkpoint
 
-
 def pretty_print_dictionary(dictionary):
     return json.dumps(dictionary, indent=2, default=str, sort_keys=True)
 
+def jsonify_checkpoint(checkpoint_path):
+    checkpoint = load_torch_checkpoint(checkpoint_path)
+    checkpoint = replace_tensors_with_placeholders(checkpoint)
+    return pretty_print_dictionary(checkpoint)
+
+def check_folder_exists(folder_path):
+    if not os.path.isdir(folder_path):
+        print(f"Folder `{folder_path}` does not exist!")
+        sys.exit(-1)
+
+
+def jsonify_checkpoints_in_folder(folder_path, output_path):
+    check_folder_exists(folder_path)
+    check_folder_exists(output_path)
+
+    for filename in os.listdir(folder_path):
+        f = os.path.join(folder_path, filename)
+        if os.path.isfile(f):
+            output_filename = os.path.join(output_path, f'{".".join(os.path.basename(f).split(".")[:-1])}.json')
+            print(f"Jsonifying checkpoint `{f}` to file `{output_filename}`")
+            j = jsonify_checkpoint(f)
+            with open(output_filename, "w+") as g:
+                g.write(j)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -27,32 +48,38 @@ if __name__ == "__main__":
         "checkpoint",
         metavar="checkpoint",
         type=str,
-        help="the path to the pretrained checkpoint",
+        help="the path to the pretrained checkpoint (or a folder containing multiple checkpoints)",
     )
     parser.add_argument(
-        "-f", "--output-file", type=str, default="", help="write dictionary to a file"
+        "-o", "--output-path", type=str, default="", help="file (or directory if inspecting multiple checkpoints) to write jsonified checkpoint to"
     )
     args = parser.parse_args()
 
     checkpoint_path = args.checkpoint
-    output_file = args.output_file
+    output_path = args.output_path
 
-    # ensure checkpoint file exists
-    if not os.path.isfile(checkpoint_path):
-        print(f"Checkpoint file {checkpoint_path} does not exist")
-        exit()
-    if os.path.isfile(output_file):
-        r = input(
-            f"Warning: output file {output_file} already exists! Are you ok with overwriting [y,n]? "
-        )
-        if r != "y":
-            exit()
-
-    checkpoint = load_torch_checkpoint(checkpoint_path)
-    checkpoint = replace_tensors_with_placeholders(checkpoint)
-    checkpoint = pretty_print_dictionary(checkpoint)
-    if output_file != "":
-        with open(output_file, "w+") as f:
-            f.write(checkpoint)
+    if os.path.isdir(checkpoint_path):
+        if output_path == "":
+            print("Error: To jsonify multiple checkpoints at once, you need to pass a output directory with -o <OUTPUT_DIR>")
+            sys.exit(-1)
+        else:
+            check_folder_exists(checkpoint_path)
+        jsonify_checkpoints_in_folder(checkpoint_path, output_path)
     else:
-        print(checkpoint)
+        # ensure checkpoint file exists
+        if not os.path.isfile(checkpoint_path):
+            print(f"Checkpoint file `{checkpoint_path}` does not exist")
+            exit()
+        if os.path.isfile(output_file):
+            r = input(
+                f"Warning: output file `{output_file}` already exists! Are you ok with overwriting [y,n]? "
+            )
+            if r != "y":
+                exit()
+
+        checkpoint = jsonify_checkpoint(checkpoint_path)
+        if output_file != "":
+            with open(output_file, "w+") as f:
+                f.write(checkpoint)
+        else:
+            print(checkpoint)
