d2c17fdbf1bf86bd3651847660372085e11f2b33
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-10-15 16:12:08 -0400

    fix docker path for fairseq

--- benchmark/fairseq/docker/run.sh
+++ benchmark/fairseq/docker/run.sh
@@ -7,5 +7,5 @@ cd "${BASH_SOURCE[0]%/*}"
 docker run -it --entrypoint bash --gpus all --ipc="host" \
 	-v "$(pwd)"/../scripts/run_eval.sh:/workspace/fairseq/run_eval.sh \
 	-v "$(pwd)"/../scripts/run_inference.sh:/workspace/fairseq/run_inference.sh \
-	-v "$(pwd)"/data:/mnt \
+	-v "$(pwd)"/../data:/mnt \
 	fairseq:latest
