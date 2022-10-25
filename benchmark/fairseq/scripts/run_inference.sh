2bf79980250175b26409dfeb11e6cb3ba69f233a
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-10-25 21:20:39 +0000

    debugging

--- benchmark/fairseq/scripts/run_inference.sh
+++ benchmark/fairseq/scripts/run_inference.sh
@@ -4,15 +4,17 @@ set -e
 DATA_PATH=/mnt/data-bin/wikitext-103
 MODEL_PATH=/mnt/en_moe_lm_15b/model.pt
 #export NCCL_DEBUG=INFO
-# python -m fairseq_cli.generate $DATA_PATH \
-#   --path $MODEL_PATH \
-#   --gen-subset valid \
-#   --sample-break-mode none \
-#   --tokens-per-sample 2048 \
-#   --batch-size 1 \
-#   --fp16 \
-#   --output-word-probs \
-#   --is-moe \
-#   --distributed-world-size 4 \
-#   --max-valid-steps 4 \
-#   --model-overrides "{'world_size': 4, 'moe_eval_capacity_token_fraction': 0.05}"
+python -m fairseq_cli.generate $DATA_PATH \
+  --path $MODEL_PATH \
+  --gen-subset valid \
+  --tokens-per-sample 2048 \ 
+  --batch-size 1 \
+  --fp16 \
+  --is-moe \
+  --distributed-world-size 4 \
+  --max-valid-steps 4 \
+  --model-overrides "{'world_size': 4, 'moe_eval_capacity_token_fraction': 0.05}" \
+  --task=language_modeling \
+  --pad-to-fixed-length \
+
+#--sample-break-mode none  --output-word-probs \
